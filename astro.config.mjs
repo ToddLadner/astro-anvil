de63329d86b4ec7ff71e407357604e8279e503ca
ToddLadner <tladner03@gmail.com>
2024-07-18 13:58:33 -0500

    added astro-imagekit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
-import icon from "astro-icon";
 
+import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
+import imagekit from "astro-imagekit";
+
+import dotenv from 'dotenv';
+dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), mdx()]
+  integrations: [icon(), mdx(), imagekit()]
 });
